440fafc1bc7f2e6d7f3e5fa87cc024fac6c761b8
Grasio Carel <100204669+grasio174@users.noreply.github.com>
2023-03-01 10:50:03 +0800

    Add files via upload
    ini merupakan tugas pertama dari dicoding

---
+++ Tugas Web BAsic/js/script.js
@@ -0,0 +1,20 @@
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+};
+
+let slides = document.querySelectorAll('.home-bg .home .slide-container .slide');
+let index = 0;
+let accordion = document.querySelectorAll('.faq .accordion-container .accordion');
+
+accordion.forEach(acco =>{
+   acco.onclick = () =>{
+      accordion.forEach(remove => remove.classList.remove('active'));
+      acco.classList.add('active');
+   }
+});
